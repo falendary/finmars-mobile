d33bb88c84eb24e1374390736c9f74388432e629
Andrei Priakhin <strelok1415@gmail.com>
2023-04-14 15:14:50 +0400

    - deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 var app = express()
 var port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use( express.static('dist') );
 
 app.listen(port, function () {
     console.info('Server started at '+port+' port');
