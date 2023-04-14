48a5a1827e99367b84385481c7450eb7a40f3500
Andrei Priakhin <strelok1415@gmail.com>
2023-04-14 15:58:31 +0400

    - deploy

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import * as path from 'path'
 
 var app = express()
 var port = process.env.PORT || 8080;
+var BASE_URL = process.env.BASE_URL || '/==BASE_URL==';
+const __dirname = path.resolve();
 
 app.use( express.static('dist') );
+app.use('*', express.static('dist'))
+
+// app.get(BASE_URL, (request, response) => {
+//     console.log('request:', request)
+//     response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+//     response.header('Expires', '-1');
+//     response.header('Pragma', 'no-cache');
+//     response.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
 
 app.listen(port, function () {
     console.info('Server started at '+port+' port');
